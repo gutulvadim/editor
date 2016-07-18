956adc369f1b68139c41c3b50bef587cb8c5f149
gutulvadim <gutul.vadim@mail.ru>
2016-07-18 11:35:40 +0300

    Create panel and canvas
    Make shapes dragable

---
+++ src/components/ShapeViewer.tsx
@@ -0,0 +1,28 @@
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
+
+class ShapeViewer extends React.Component<any, any> {
+    constructor(props?, context?) {
+        super(props, context);
+    }
+    render() {
+        var shapes = this.props.shapes.map(function(s) {{}
+            switch (s.name) {
+                case 'circle': return <Circle x={s.x} y={s.y} r={s.r} key={s.id} id={s.id} p={this.props.updateShape} />;
+                case 'rectangle': return <Rectangle x={s.x} y={s.y} width={s.w} height={s.h} key={s.id} id={s.id} p={this.props.updateShape} />;
+            }
+        }.bind(this));
+
+        return(<svg>{shapes}</svg>);
+    }
+}
+
+export default connect(
+    (state) => ({ shapes: state.shapes }),
+    (dispatch) => ({
+        updateShape: (id, x, y) => dispatch({ type:'SHAPE_CHANGE', id, x, y})
+    })
+)(ShapeViewer);
