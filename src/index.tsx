ce5547ac5ba7011b530738b5aab6205496c6a372
Eugene Barhatov <barhatov@gmail.com>
2016-07-21 13:30:34 +0300

    Specify types inside a reducer

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import * as ReactDOM from "react-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Pane }  from "./components/pane";
-import shapeReducer from './reducers/shape_reducer';
+import shapeReducer, { IShapeState } from './reducers/shape_reducer';
 
-let defaultState = { nextShapeId:0, paletteWidth:60, shapeWidth:100, shapeHeight:100, shapes:[] };
+let defaultState: IShapeState = { 
+  nextShapeId: 0, paletteWidth: 60, shapeWidth: 100, shapeHeight: 100, shapes: [] };
 let store = createStore(shapeReducer, defaultState);
 
 ReactDOM.render(
--- src/reducers/shape_reducer.ts
+++ src/reducers/shape_reducer.ts
@@ -1,12 +1,23 @@
-export enum Actions
-{
+import { IShapeData } from '../components/shapes/shape';
+import { ICanvasState } from '../components/canvas';
+
+export enum Actions {
   SHAPE_ADD,
   SHAPE_MOVE,
   SHAPE_BACK,
   SHAPE_FORWARD
 }
 
-export default (state, action) => {
+export interface IShapeAction extends IShapeData {
+  type: Actions;
+}
+
+export interface IShapeState extends ICanvasState { 
+  nextShapeId: number;
+  paletteWidth: number;
+}
+
+export default (state: IShapeState, action: IShapeAction): IShapeState => {
   switch (action.type) {
     case Actions.SHAPE_ADD:
       return addShape(state, action);
@@ -31,7 +42,7 @@ function getShapeIndex(shapes, id) {
   return shapes.indexOf(shape);
 }
 
-function addShape(state, action) {
+function addShape(state: IShapeState, action: IShapeAction): IShapeState {
   let id = state.nextShapeId;
   let shape = Object.assign({}, {id: id}, action);
   shape.x -= state.paletteWidth;
@@ -39,33 +50,33 @@ function addShape(state, action) {
   return Object.assign({}, state, {nextShapeId: id + 1, shapes: [...state.shapes, shape]});
 }
 
-function moveShape(state, action) {
+function moveShape(state: IShapeState, action: IShapeAction): IShapeState {
   let shapeIndex = getShapeIndex(state.shapes, action.id);
-  let new_shape = Object.assign({}, state.shapes[shapeIndex], {x: action.x, y: action.y});
-  let new_shapes = [...state.shapes];
-  new_shapes[shapeIndex] = new_shape;
+  let newShape = Object.assign({}, state.shapes[shapeIndex], {x: action.x, y: action.y});
+  let newShapes = [...state.shapes];
+  newShapes[shapeIndex] = newShape;
 
-  return Object.assign({}, state, {shapes: new_shapes});
+  return Object.assign({}, state, {shapes: newShapes});
 }
 
-function shapeBack(state, action) {
+function shapeBack(state: IShapeState, action: IShapeAction): IShapeState {
   let shapeIndex = getShapeIndex(state.shapes, action.id);
   if (shapeIndex == 0 || state.shapes.length <= 1) {
     return state;
   }
-  let new_shapes = [...state.shapes];
-  [new_shapes[shapeIndex - 1], new_shapes[shapeIndex]] = [new_shapes[shapeIndex], new_shapes[shapeIndex - 1]];
+  let newShapes = [...state.shapes];
+  [newShapes[shapeIndex - 1], newShapes[shapeIndex]] = [newShapes[shapeIndex], newShapes[shapeIndex - 1]];
 
-  return Object.assign({}, state, {shapes: new_shapes});
+  return Object.assign({}, state, {shapes: newShapes});
 }
 
-function shapeForward(state, action) {
+function shapeForward(state: IShapeState, action: IShapeAction): IShapeState {
   let shapeIndex = getShapeIndex(state.shapes, action.id);
   if (state.shapes.length <= 1 || shapeIndex == state.shapes.length - 1) {
     return state;
   }
-  let new_shapes = [...state.shapes];
-  [new_shapes[shapeIndex + 1], new_shapes[shapeIndex]] = [new_shapes[shapeIndex], new_shapes[shapeIndex + 1]];
+  let newShapes = [...state.shapes];
+  [newShapes[shapeIndex + 1], newShapes[shapeIndex]] = [newShapes[shapeIndex], newShapes[shapeIndex + 1]];
 
-  return Object.assign({}, state, {shapes: new_shapes});
+  return Object.assign({}, state, {shapes: newShapes});
 }
